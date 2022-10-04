5df99f93487de370ac0b79f4be0341a30a5aefe5
Andrey Trubitsyn <andrejtrubicyn@Andrejs-MacBook-Air.local>
2022-10-04 12:05:30 +0400

    form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,61 @@
-import React from 'react'
-import './Form.css'
+import React, { useEffect, useState } from "react";
+import { useTelegram } from "../../hooks/useTelegram";
+import "./Form.css";
 
 export const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+  const [country, setCountry] = useState("");
+  const [street, setStreet] = useState("");
+  const [face, setFace] = useState("physical");
 
+  const onChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
+
+  const onChangeStreet = (e) => {
+    setStreet(e.target.value);
+  };
+
+  const onChangeFace = (e) => {
+    setFace(e.target.value);
+  };
+
+  const { tg } = useTelegram();
+
+  useEffect(() => {
+    tg.MainButton.setParams({
+        text: 'Отправить данные'
+    })
+  }, [])
+
+  useEffect(() => {
+    if (!street || !country) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.show()
+    }
+  }, [country, street, tg.MainButton])
+
+  return (
+    <form className="form">
+      <h3>Введите ваши данные</h3>
+      <input
+        className="input"
+        placeholder="country"
+        type="text"
+        onChange={onChangeCountry}
+        value={country}
+      />
+      <input
+        className="input"
+        placeholder="street"
+        type="text"
+        onChange={onChangeStreet}
+        value={street}
+      />
+      <select className="select" value={face} onChange={onChangeFace}>
+        <option value={"physical"}>Физ. лиц</option>
+        <option value={"legal"}>Юр. лицо</option>
+      </select>
+    </form>
+  );
+};

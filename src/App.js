bd56b5648bf8239320e68d29b32325afcbfa0138
Andrey Trubitsyn <andrejtrubicyn@Andrejs-MacBook-Air.local>
2022-10-04 10:39:52 +0400

    fixes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const { onToggleButton, tg } = useTelegram();
   useEffect(() => {
     tg.ready();
-  }, []);
+  }, [tg]);
 
   return (
     <div className="App">
---
+++ src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Form.css'
+
+export const Form = () => {
+  return (
+    <div>Form</div>
+  )
+}
+

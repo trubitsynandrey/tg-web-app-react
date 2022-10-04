5fd283438ec56b3a14a59d16335fda7905d3b86b
Andrey Trubitsyn <andrejtrubicyn@Andrejs-MacBook-Air.local>
2022-10-04 10:49:39 +0400

    feat: routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Form } from "./components/Form/Form";
 import { Header } from "./components/Header/Header";
+import { ProductList } from "./components/ProductList/ProductList";
 import { useTelegram } from "./hooks/useTelegram";
 
-
-
 function App() {
-  const { onToggleButton, tg } = useTelegram();
+  const { tg } = useTelegram();
   useEffect(() => {
     tg.ready();
   }, [tg]);
@@ -14,7 +15,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element={<ProductList />}/>
+        <Route path={'form'} element={<Form />}/>
+      </Routes>
     </div>
   );
 }
